87cc412dde06f1edacdd5f1f073b4e64fecc054c
Muhammed Ali ASLAN <muhammedaliaslanmail@gmail.com>
2023-09-22 18:20:48 +0300

    İstatistik eklemesi Yapıldı

--- BusinessLayer/Concrete/CategoryManager.cs
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -1,4 +1,7 @@
-﻿using DataAccessLayer.Concrete.Repositories;
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete.Repositories;
 using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
@@ -8,24 +11,43 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Concrete
 {
-    public class CategoryManager
+    public class CategoryManager : ICategoriService
     {
-        GenericRepository<Category> repo = new GenericRepository<Category>();
-        public List<Category> GetAllBL()
+        ICategoryDal _categorydal;
+
+        public CategoryManager(ICategoryDal categorydal)
         {
-            return repo.List();
+            _categorydal = categorydal;
         }
-        public void CategoryAddBL(Category p)
+
+        public void CategoryAdd(Category category)
         {
-            if (p.CategoryName == "" || p.CategoryName.Length <= 3 || p.CategoryDescription == "" || p.CategoryName.Length >= 51)
-            {
-                //hata mesajı
-                Console.WriteLine("Hata");
-            }
-            else
-            {
-                repo.Insert(p);
-            }
+            _categorydal.Insert(category);
         }
+
+        public void CategoryDelete(Category category)
+        {
+            _categorydal.Delete(category);
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+            _categorydal.Update(category);
+        }
+
+        public Category GetByID(int id)
+        {
+            return _categorydal.Get(x => x.CategoryID == id);
+        }
+
+        public List<Category> GetList()
+        {
+            return _categorydal.List();
+        }
+        public CategoryValidator GetValidator()
+        {
+            return new CategoryValidator();
+        }
+
     }
 }
---
+++ MvcProjeKampi/Controllers/istatistikController.cs
@@ -0,0 +1,46 @@
+﻿using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class istatistikController : Controller
+    {
+        Context c = new Context();
+        public ActionResult Istatistikler()
+        {
+
+            //1) Toplam kategori sayısı
+            var TotalCategory = c.Categories.Count();
+            ViewBag.TotalCategoryNumber = TotalCategory;
+
+            //2) Başlık tablosunda "yazılım" kategorisine ait başlık sayısı
+            var SoftwareCategory = c.Headings.Count(x => x.Category.CategoryName == "Yazılım");
+            ViewBag.TotalSoftwareCategory = SoftwareCategory;
+
+            //3) Yazar adında 'a' harfi geçen yazar sayısı
+            var WriterName = c.Writers.Count(x => x.WriterName.Contains("a"));
+            ViewBag.WriterNameA = WriterName;
+
+            //4) En fazla başlığa sahip kategori adı
+
+            var MaxCategory = c.Headings.Max(x => x.Category.CategoryName);
+            ViewBag.MaxCategoryName = MaxCategory;
+
+            //5) Kategori tablosunda durumu true olan kategoriler ile false olan kategoriler arasındaki sayısal fark
+            int sonuc = 0;
+
+            var CategoryStatusTrue = c.Categories.Count(x => x.CategoryStatus == true);
+            var CategoryStatusFalse = c.Categories.Count(y => y.CategoryStatus == false);
+
+            sonuc = (CategoryStatusTrue - CategoryStatusFalse);
+
+            ViewBag.fark = sonuc;
+
+            return View();
+        }
+    }
+}
